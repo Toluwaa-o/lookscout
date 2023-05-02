6373c767995bae7a7a365942104f25d39f682160
toluwa <121364075+Toluwaa-o@users.noreply.github.com>
2023-05-02 01:31:04 +0000

    tweak

--- src/container/pages.js
+++ src/container/pages.js
@@ -1,6 +1,13 @@
-import { Outlet, Navigate } from "react-router"
+import { Outlet, useNavigate } from "react-router"
+import { useEffect } from 'react
 
 export default function pages() {
+  const navigate = useNavigate()
+  
+  useEffect(() => {
+    navigate('register')
+  }, [])
+  
   return (
     <>
       <Outlet />
